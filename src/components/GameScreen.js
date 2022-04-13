6f83906696626eca8d943ffad2201c2df96e21f9
Minh Pham <minh.pham@exxeta.com>
2022-04-13 11:05:14 +0200

    Added Game-Bg and button

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -1,6 +1,17 @@
 function GameScreen() {
   return (
-    <div className="App">
+    <div className="game-screen-container">
+      <img
+        src="/images/bg-game.svg"
+        alt="Green field"
+      />
+
+      <button className="plus-button">
+        <img
+          src="/images/plus-icon.svg"
+          alt="Plus Icon"
+        />
+      </button>
       <div>GAME SCREEN</div>
     </div>
   );
