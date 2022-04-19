21528d9d8f92a1e6f09a514497050bbf36e7c677
Manuela Müller <manuela.mueller@exxeta.com>
2022-04-19 14:43:24 +0200

    * Change background image (bg-hills.svg) on start screen to Stuttgart version * Add bee and more leaves * Adjust layout on start screen of Stuttgart

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -11,9 +11,23 @@ function StartScreen({ setIsGameScreen }) {
         src="/images/bg-leaves.svg"
         alt="Three leaves"
       />
-
+      <img
+        className="bg-leaves-2"
+        src="/images/bg-leaves-2.svg"
+        alt="Three leaves"
+      />
+      <img
+        className="stuttgart-small-tree"
+        src="/images/tree-small.svg"
+        alt="a small tree"
+      />
+      <img
+        className="bee"
+        src="/images/bee.svg"
+        alt="a cute bee"
+      />
       <div className="start-screen-textbox">
-        <h3>Lorem Ipsum<br />dolor sit amet</h3>
+        <h3>Mehr Bienen für<br />deine Stadt!</h3>
         <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
         <button className="start-button" onClick={() => setIsGameScreen(true)}>
           Starten
