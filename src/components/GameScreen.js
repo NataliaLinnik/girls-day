cc83bccec2efcec2fba2532a60da03c1c66e2271
Minh Pham <minh.pham@exxeta.com>
2022-04-20 11:45:10 +0200

    * Added "EndScreen" * Hides Timer and Clicker after 15sec * Renders Endscreen after 15sec

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component, useEffect, useRef, useState } from 'react';
 
 function GameScreen() {
   return (
@@ -7,65 +7,117 @@ function GameScreen() {
         src="/images/bg-game.svg"
         alt="Green field"
       />
-      <ClickCounter/>
-      <CountDownTimer/>
+      <CountDownTimer />
+      <HideIn15Seconds />
+      <RenderIn15Seconds />
     </div>
   );
 }
 
 function CountDownTimer() {
-  const [timer, setTimer] = React.useState(5);
-  const id =React.useRef(null);
-  const clear=()=>{
-  window.clearInterval(id.current)
-}
-  React.useEffect(()=>{
-     id.current=window.setInterval(()=>{
-      setTimer((time)=>time-1)
-    },1000)
-    return ()=>clear();
-  },[])
+  const [timer, setTimer] = useState(15);
+  const id = useRef(null);
+  const clear = () => {
+    window.clearInterval(id.current)
+  }
+  const [showComponent, setShowComponent] = useState(true);
 
-  React.useEffect(()=>{
-    if(timer===0){
+  useEffect(() => {
+    id.current = window.setInterval(() => {
+      setTimer((time) => time - 1)
+    }, 1000)
+    return () => clear();
+  }, [])
+
+  useEffect(() => {
+    if (timer === 0) {
       clear()
     }
+  }, [timer])
 
-  },[timer])
+  useEffect(() => {
+    setInterval(() => {
+      setShowComponent(false);
+    }, 5000);
+  }, []);
 
-  return (
-    <div className="countdown">{timer}
-      <br/> SEKUNDEN
-    </div>
-  );
+  return <>
+    {showComponent && <div>
+      <div className="countdown">{timer}
+        <br /> SEKUNDEN
+      </div></div>}
+  </>
 }
 
-class ClickCounter extends React.Component {
+class ClickCounter extends Component {
   constructor(props) {
     super(props);
-    this.state = {value: 0};
+    this.state = { value: 0 };
     this.buttonClicked = this.buttonClicked.bind(this);
   }
-  
+
   buttonClicked(event) {
-    this.setState({value: this.state.value + 1});
+    this.setState({ value: this.state.value + 1 });
   }
-  
+
   render() {
     return (
-    	<div>
+      <div>
         <div className='counter'>{this.state.value}
-          <br/> CLICKS
+          <br /> CLICKS
         </div>
         <button className="plus-button" onClick={this.buttonClicked}>
-        <img
-          src="/images/plus-icon.svg"
-          alt="Plus Icon"
-        />
-      </button>
+          <img
+            src="/images/plus-icon.svg"
+            alt="Plus Icon"
+          />
+        </button>
       </div>
     );
   }
+}
+
+class EndScreen extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className='success'>Gut gemacht!</div>
+        <button className='whatsapp-button'>Zurück zu WhatsApp</button>
+        <button className='share-button'>Ergebnis teilen</button>
+      </div>
+    )
+  }
+}
+
+const HideIn15Seconds = () => {
+  const [showComponent, setShowComponent] = useState(true);
+
+  useEffect(() => {
+    setInterval(() => {
+      setShowComponent(false);
+    }, 5000);
+  }, []);
+
+  return <> {
+    showComponent &&
+    <ClickCounter />
+  }
+  </>
+}
+
+const RenderIn15Seconds = () => {
+  const [showComponent, setShowComponent] = useState(false);
+
+  useEffect(() => {
+    setInterval(() => {
+      setShowComponent(true);
+    }, 5000);
+  }, []);
+
+  return <> {
+    showComponent && <EndScreen />
+  }
+  </>
 }
 
 export default GameScreen;
