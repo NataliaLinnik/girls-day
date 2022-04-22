f390851c6bdc16ad7d9d3ae9c1cbf47aed799ad6
Manuela Müller <manuela.mueller@exxeta.com>
2022-04-22 16:46:10 +0200

    * Fix start screen background

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,17 +1,7 @@
 function StartScreen({ setIsGameRunning }) {
   return (
     <div className="start-screen-container">
-      <img
-        className="bg-hills"
-        src="/images/bg-hills.svg"
-        alt="Green hills"
-      />
-      <img
-        className="bg-leaves"
-        src="/images/bg-leaves.svg"
-        alt="Three leaves"
-      />
-
+      <StartScreenBackground />
       <div className="start-screen-textbox">
         <h3>Lorem Ipsum<br />dolor sit amet</h3>
         <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
@@ -20,6 +10,23 @@ function StartScreen({ setIsGameRunning }) {
         </button>
       </div>
     </div >
+  );
+}
+
+function StartScreenBackground() {
+  return (
+    <div className="start-screen-background">
+      <img
+        className="bg-hills"
+        src="/images/bg-hills.svg"
+        alt="Green hills"
+      />
+      <img
+        className="bg-leaves-1"
+        src="/images/bg-leaves.svg"
+        alt="Three leaves"
+      />
+    </div>
   );
 }
 
