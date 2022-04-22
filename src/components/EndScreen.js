619ea9d0837057a99b8c3bab6b5d85e9abb335fb
Minh Pham <minh.pham@exxeta.com>
2022-04-22 16:03:30 +0200

    Resolved merge conflict

--- src/components/EndScreen.js
+++ src/components/EndScreen.js
@@ -1,9 +1,13 @@
 function EndScreen({ counter }) {
     return (
-        <div>
+        <div className="end-screen-container">
+            <img
+                src="/images/bg-game.svg"
+                alt="Green field"
+            />
             <div>{counter}</div>
             <div className='success'>Gut gemacht!</div>
-            <button className='whatsapp-button'>Nochmal spielen</button>
+            <button className='play-again-button'>Nochmal spielen</button>
             <button className='share-button'>Ergebnis teilen</button>
         </div>
     )
