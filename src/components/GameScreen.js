f92e62fef5cbf19cfd32dfb25f1e5a5f11a8cb98
Linnik, Natalia <Natalia.Linnik@EXXETA.com>
2022-04-24 20:44:30 +0200

    remove plus button, added click on whole screen, added styling for counters on game screen

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -3,15 +3,11 @@ import React, { useEffect, useRef, useState } from 'react';
 function GameScreen({ counter, setCounter }) {
   const buttonClicked = () => {
     setCounter(counter + 1);
-  }
+  };
 
   return (
-    <div className="game-screen-container">
-      <img
-        className="bg-hills"
-        src="/images/bg-hills.svg"
-        alt="Green hills"
-      />
+    <div className="game-screen-container" onClick={buttonClicked}>
+      <img className="bg-hills" src="/images/bg-hills.svg" alt="Green hills" />
       <img
         className="bg-leaves"
         src="/images/bg-leaves.svg"
@@ -19,16 +15,11 @@ function GameScreen({ counter, setCounter }) {
       />
       <CountDownTimer />
       <div>
-      <div className='counter'>{counter}
-        <br /> CLICKS
+        <div className="counter-container">
+          <div className="counter">{counter}</div>
+          <div className="clicks-text">CLICKS</div>
+        </div>
       </div>
-      <button className="plus-button" onClick={buttonClicked}>
-        <img
-          src="/images/plus-icon.svg"
-          alt="Plus Icon"
-        />
-      </button>
-    </div>
     </div>
   );
 }
@@ -37,26 +28,28 @@ function CountDownTimer() {
   const [timer, setTimer] = useState(15);
   const id = useRef(null);
   const clear = () => {
-    window.clearInterval(id.current)
-  }
+    window.clearInterval(id.current);
+  };
 
   useEffect(() => {
     id.current = window.setInterval(() => {
-      setTimer((time) => time - 1)
-    }, 1000)
+      setTimer(time => time - 1);
+    }, 1000);
     return () => clear();
-  }, [])
+  }, []);
 
   useEffect(() => {
     if (timer === 0) {
-      clear()
+      clear();
     }
-  }, [timer])
+  }, [timer]);
 
   return (
-    <div className="countdown">{timer}
-      <br /> SEKUNDEN</div>
-  )
+    <div className="countdown">
+      <div className="countdown-timer">{timer}</div>
+      <div className="countdown-text">SEKUNDEN</div>
+    </div>
+  );
 }
 
 export default GameScreen;
