349e3e0bce6a0786fa3c3f17cc0e258b52974f91
Manuela Müller <manuela.mueller@exxeta.com>
2022-04-25 12:55:43 +0200

    Add flowers randomly (wip)

--- src/components/GameScreen.js
+++ src/components/GameScreen.js
@@ -1,11 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CountDownTimer from './CountDownTimer';
 
 function GameScreen({ counter, setCounter }) {
   const [startGame, setStartGame] = useState(false);
+  const [flowers] = useState([]);
 
   const buttonClicked = () => {
     setCounter(counter + 1);
+    const xPosition = Math.random() * 100;
+    const yPosition = Math.random() * 100;
+    const height = (yPosition) => {
+      if (yPosition < 30) {
+        return 20
+      } else if (yPosition >= 30 && yPosition < 60) {
+        return 30
+      } else {
+        return 40
+      }
+    };
+    flowers.push(new Flower(
+      (counter),
+      "/images/flowers/flower-1.svg",
+      xPosition,
+      yPosition,
+      height(yPosition)
+    ));
   };
 
   return (
@@ -32,15 +51,15 @@ function GameScreen({ counter, setCounter }) {
           />
         </div>
       )}
-      <img className="bg-hills" src="/images/bg-hills.svg" alt="Green hills" />
+      <div className='game-end-screen-background'>
+        <img className="bg-hills" src="/images/bg-hills-game.svg" alt="Green hills" />
+        <img className="bg-hill-counter" src="/images/bg-hill-counter.svg" alt="A small hill" />
+      </div>
+
+      {startGame && <FlowerMeadow flowers={flowers} />}
 
       {startGame && (
         <>
-          <img
-            className="bg-leaves"
-            src="/images/bg-leaves.svg"
-            alt="Three leaves"
-          />
           <CountDownTimer
             value={15}
             showText
@@ -61,3 +80,38 @@ function GameScreen({ counter, setCounter }) {
 
 export default GameScreen;
 
+function FlowerMeadow({ flowers }) {
+
+  const flowerStyle = (positionX, positionY, height) => ({
+    position: `absolute`,
+    left: positionX + "%",
+    top: positionY + "%",
+    height: height,
+  });
+
+  return (
+    <div className="flower-meadow">
+      <div>
+        {flowers.map((flower) => {
+          return (
+            <img src={flower.asset} key={flower.key} style={flowerStyle(
+              flower.positionX,
+              flower.positionY,
+              flower.height
+            )} />
+          )
+        })}</div>
+    </div>
+  );
+}
+
+
+export class Flower {
+  constructor(key, asset, positionX, positionY, height) {
+    this.key = key;
+    this.asset = asset;
+    this.positionX = positionX;
+    this.positionY = positionY;
+    this.height = height;
+  }
+}
