51d9893e98320eb3098f4dd2817222391c25938e
Manuela Müller <manuela.mueller@exxeta.com>
2022-04-25 17:11:53 +0200

    Add hill for click counter (GameScreen & EndScreen)

--- src/components/EndScreen.js
+++ src/components/EndScreen.js
@@ -4,17 +4,18 @@ function EndScreen({ counter, setShowInfoPage }) {
   return (
     <div className="end-screen-container">
       <InfoIcon setShowInfoPage={setShowInfoPage} />
-      <img className="bg-hills" src="/images/bg-hills.svg" alt="Green hills" />
-      <img
-        className="bg-leaves"
-        src="/images/bg-leaves.svg"
-        alt="Three leaves"
-      />
+      <div className="game-end-screen-background">
+        <img className="bg-hills" src="/images/bg-hills.svg" alt="Green hills" />
+        <img className="bg-hill-counter-2" src="/images/bg-hill-counter.svg" alt="A small hill" />
+        <div className="square" />
+      </div>
       <div>{counter}</div>
       <div className="success">Gut gemacht!</div>
-      <button className="play-again-button">Nochmal spielen</button>
-      <button className="share-button">Ergebnis teilen</button>
-    </div>
+      <div className="evaluation">
+        <button className="play-again-button">Nochmal spielen</button>
+        <button className="share-button">Ergebnis teilen</button>
+      </div>
+    </div >
   );
 }
 
