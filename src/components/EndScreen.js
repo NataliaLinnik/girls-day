253a289fb922f045a474b9da6c0ccea8a67b6d11
Linnik, Natalia <Natalia.Linnik@EXXETA.com>
2022-04-25 17:22:21 +0200

    small styling improvements

--- src/components/EndScreen.js
+++ src/components/EndScreen.js
@@ -1,20 +1,24 @@
 import InfoIcon from './InfoIcon';
 
-function EndScreen({ counter, setShowInfoPage}) {
+function EndScreen({ counter, setShowInfoPage }) {
   return (
     <div className="end-screen-container">
       <InfoIcon setShowInfoPage={setShowInfoPage} />
       <img className="bg-hills" src="/images/bg-hills.svg" alt="Green hills" />
-      <img
-        className="bg-leaves"
-        src="/images/bg-leaves.svg"
-        alt="Three leaves"
-      />
-      <div>{counter}</div>
+
       <div className="success">Gut gemacht!</div>
-      <div className="successDescription">Dein Baum ist um {counter} cm gewachsen. Das entspricht einer Spende von</div>
-      <div className="successPrice"> {counter / 10} EUR</div>
-      <button className="play-again-button" onClick={ () => {window.location.reload()}}>Nochmal spielen</button>
+      <div className="success-description">
+        Dein Baum ist um {counter} cm gewachsen. Das entspricht einer Spende von
+      </div>
+      <div className="success-price"> {counter / 10} EUR</div>
+      <button
+        className="play-again-button"
+        onClick={() => {
+          window.location.reload();
+        }}
+      >
+        Nochmal spielen
+      </button>
       <button className="share-button">Ergebnis teilen</button>
     </div>
   );
