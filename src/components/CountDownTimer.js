f7186f3bc31aaa740253c4d9168e0ec3862e9355
Manuela Müller <manuela.mueller@exxeta.com>
2022-04-28 08:33:24 +0200

    * Clean up * Timer Bug

--- src/components/CountDownTimer.js
+++ src/components/CountDownTimer.js
@@ -18,7 +18,7 @@ function CountDownTimer({
   useEffect(() => {
     if (startTimer) {
       id.current = window.setInterval(() => {
-        setTimer(time => time - 1);
+        setTimer(time => time - 2);
       }, 1000);
     }
     return () => clear();
